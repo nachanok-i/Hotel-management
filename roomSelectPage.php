e3f3cf1557c7c2f3dd94241f574ba309675b488d
GuardmanHowdy <book21424@gmail.com>
2020-04-26 20:55:25 +0700

    Update roomSelectPage.php

--- roomSelectPage.php
+++ roomSelectPage.php
@@ -6,8 +6,8 @@ session_start();
 <!Doctype html>
 <html>
     <head>
-        <meta charset ="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset ="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" type="text/css" href="roomSelectPage.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -17,6 +17,7 @@ session_start();
         <!--This is my kit font awesome pls remind me-------------------------------------------->
         <script src="https://kit.fontawesome.com/92d742c429.js" crossorigin="anonymous"></script>
         <!--------------------------------------------------------------------------------------->
+        
     </head>
     <body>
      
