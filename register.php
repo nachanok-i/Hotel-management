26ca02cf2745be88b0e159b1a1efa5e75754710f
GuardmanHowdy <book21424@gmail.com>
2020-05-17 13:25:59 +0700

    update required state

--- register.php
+++ register.php
@@ -160,7 +160,7 @@ if (isset($_SESSION['email']) != NULL) {
         </div>
         <div class="form-group col-sm-3 my-1">
           <label for="state"><b>State</b></label>
-          <input type="text" class="form-control form-control-lg" name="state" id="state" placeholder="Enter State (Optional)">
+          <input type="text" class="form-control form-control-lg" name="state" id="state" placeholder="Enter State" required>
         </div>
       </div>
       <div class="form-row">
