f092dcf6b962875e18ad5f0ed6a491e335f1aeb3
nachanok-tap <nachanok.tap@users.noreply.github.com>
2020-05-18 16:30:52 +0700

    decorate

--- index.php
+++ index.php
@@ -241,6 +241,8 @@ if (isset($_SESSION['email']) != NULL) {
 				$path = './customerPicture/';
 				if ($result->num_rows > 0) {
 					while ($row = mysqli_fetch_array($result)) {
+						$imgName = $row['profileImage'];
+						echo '<img src="' . $path . $imgName . '" class="img-fluid img-thumbnail mx-auto d-block" alt="customer picture" >';
 						echo '<div class="row">';
 						echo 	'<div class="col profile" style=" text-align: center;">';
 						echo 		'<h5 style="font-size:2em;"> Firstname </h5>';
@@ -344,7 +346,7 @@ if (isset($_SESSION['email']) != NULL) {
 	var win = $(window),
 		foo = $('#typer');
 
-	foo.typer(['Luxury Hotels', 'Hotels & Resorts', 'Luxury Resorts']);
+	foo.typer(['Elegant Stay', 'Endless Happiness', 'Amazing Atmosphere']);
 
 	// unneeded...
 	win.resize(function() {
