ce29a555f43f4930c0e4839d73e4a357025f3b9c
nachanok-tap <nachanok.tap@users.noreply.github.com>
2020-05-19 17:59:06 +0700

    Update staffRegister_action.php

--- staffRegister_action.php
+++ staffRegister_action.php
@@ -50,7 +50,7 @@ if (isset($_POST['submit']) ) {
     $picture =  $fileName;
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $sql = "INSERT INTO EmployeeApplication(firstName, lastName, email, requiredSalary, startDate, street,city,state,zipCode,country,nationality,phone,imgURL,gender,position,branchID)  
-            VALUES('$firstName', '$lastName', '$email', '$requireSalary','$startDate', '$street', '$city','$state','$zipCode','$country','$nationality','$phone','$fileDestination','$gender','$position','$branch')";
+            VALUES('$firstName', '$lastName', '$email', '$requireSalary','$startDate', '$street', '$city','$state','$zipCode','$country','$nationality','$phone','$fileName','$gender','$position','$branch')";
 
         if ($conn->query($sql) === TRUE) {
             $conn->close();
